b9905024115226185254150d5f80f4be22683581
AlyxerHL <alyxer@alyxer.me>
2023-07-13 17:22:41 +0900

    Add home page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { version } from "$app/environment";
+</script>
+
+<nav class="container-fluid">
+    <ul>
+        <li><a href="/" class="contrast"><b>Alyxer's FHLib</b></a></li>
+    </ul>
+    <ul>
+        <li>Version {version}</li>
+        <li>•</li>
+        <li><a href="https://github.com/AlyxerHL/gearing-tuner">Source Code</a></li>
+    </ul>
+</nav>
+<slot />
