4764f7fa02235a1903907aa1e0037ec71bc8f124
TempusGPT <contact@changyong.me>
2024-02-06 06:03:51 +0900

    Change directory structure

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,8 @@
 import "~/libs/math.ext";
 
 import { render } from "solid-js/web";
-import Layout from "~/app/Layout";
-import HomePage from "~/app/HomePage";
+import Layout from "~/pages/Layout";
+import HomePage from "~/pages/HomePage";
 
 const code = () => (
     <Layout version="2.0.0" url="https://github.com/TempusGPT/f1gt">
@@ -10,5 +10,4 @@ const code = () => (
     </Layout>
 );
 
-document.title = "Forza #1 Gearing Tuner";
 render(code, document.body);
--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,10 @@ export default defineConfig({
     },
     build: {
         target: "esnext",
+        outDir: "../dist",
+        rollupOptions: {
+            input: "./app/index.html",
+        },
     },
+    root: "./app",
 });
