7838a78372fd76e79c083d2f84bb9696ce302fd2
TempusGPT <contact@changyong.me>
2024-05-30 11:19:09 +0900

    Add readme.md

--- src/libs/intl/index.ts
+++ src/libs/intl/index.ts
@@ -1,7 +1,4 @@
 import { en } from "./intl.en";
 import { ko } from "./intl.ko";
 
-const intl = navigator.language.startsWith("ko") ? ko : en;
-
-export const homePage = intl.homePage;
-export const gearInput = intl.gearInput;
+export const { homePage, gearInput } = navigator.language.startsWith("ko") ? ko : en;
