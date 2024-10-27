088df15db36bdbb44f547b67820c1212dc9ce0bb
TempusGPT <contact@changyong.me>
2024-10-27 23:20:27 +0900

    Show range value under the thumb

--- src/libs/components/Range.svelte
+++ src/libs/components/Range.svelte
@@ -9,10 +9,41 @@
 </script>
 
 <script lang="ts">
-    let { label, min, max, step, value = $bindable() }: Props = $props();
+    let {
+        label,
+        min = 0,
+        max = 100,
+        step = 1,
+        value = $bindable((max - min) / 2),
+    }: Props = $props();
+
+    $effect(() => {
+        document.documentElement.style.setProperty(
+            "--range-value-position",
+            ((value - min) / (max - min)).toString(),
+        );
+    });
 </script>
 
-<label>
-    <div>{label} ({value})</div>
+<label class="range-container">
+    <div>{label}</div>
     <input type="range" {min} {max} {step} bind:value />
+    <small class="value">{(value / step).toFixed()}</small>
 </label>
+
+<style>
+    .range-container {
+        position: relative;
+        margin-bottom: calc(var(--pico-spacing) * 1.5);
+    }
+
+    .value {
+        position: absolute;
+        transform: translateX(-50%);
+        max-width: min-content;
+        left: calc(
+            (0% + 0.625rem) * (1 - var(--range-value-position)) + (100% - 0.625rem) *
+                var(--range-value-position)
+        );
+    }
+</style>
